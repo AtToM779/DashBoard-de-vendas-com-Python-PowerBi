a565da21735a9a345f958fb34799be4326648048
AtToM779 <matheusmotta189@gmail.com>
2026-05-30 09:47:17 -0300

    Adcionado codigo para encontrar numero de zeros em cada tabela

--- Limpeza_dados.py
+++ Limpeza_dados.py
@@ -2,17 +2,27 @@ import pandas as pd
 import glob
 import os
 from pathlib import Path
+import numpy as np
 
 # Carregamentos de todos os CSV em "arquivos"
-arquivos = Path("Dados")
 
-dfs = {}
-
-for arquivos in arquivos.glob("*.csv"):
-    nome = arquivos
-    dfs[nome] = pd.read_csv(arquivos)
-    print(f"  {nome}: {dfs[nome].shape}")
+tabelas = {
+    "orders":      pd.read_csv("Dados/olist_orders_dataset.csv"),
+    "items":       pd.read_csv("Dados/olist_order_items_dataset.csv"),
+    "products":    pd.read_csv("Dados/olist_products_dataset.csv"),
+    "customers":   pd.read_csv("Dados/olist_customers_dataset.csv"),
+    "geolocation": pd.read_csv("Dados/olist_geolocation_dataset.csv"),
+    "pagamentos":  pd.read_csv("Dados/olist_order_payments_dataset.csv"),
+    "reviews":     pd.read_csv("Dados/olist_order_reviews_dataset.csv"),
+    "vendedores":  pd.read_csv("Dados/olist_sellers_dataset.csv"),
+    "traducao":    pd.read_csv("Dados/product_category_name_translation.csv"),
+}
 
 # Limpeza dos dados
 
+for nome, df in tabelas.items():
+    nulos = df.isna().sum()
+    nulos = nulos[ nulos > 0]
+    print(f"\n{nome}")
+    print(nulos)
 
